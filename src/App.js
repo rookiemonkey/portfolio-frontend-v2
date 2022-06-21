5559886c808a204fdbb7e6617036fcb746c8c5c3
rookiemonkey <kevinroirigorbasina@pm.me>
2022-06-21 08:13:26 +0800

    :heavy_check_mark: remove halftone

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Backdrop from './hocs/Backdrop';
 import Contain from './hocs/Contain';
 import Home from './pages/Home';
-import SVGHalftone from './components/SVGHalftone';
 
 function App() {
   return (
     <Backdrop>
       <Contain>
-
-        <SVGHalftone className='halftone' leftOffset={-0.1} topOffset={0.05} />
 
         <Home />
         
