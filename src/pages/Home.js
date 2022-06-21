08530141c4ce9afa8cd16163e3b4f9ad16b6c71a
rookiemonkey <kevinroirigorbasina@pm.me>
2022-06-21 13:30:30 +0800

    :heavy_plus_sign: add animations on home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -60,8 +60,9 @@ const Home = () => {
         <div className={styles['contents-right']} ref={right}>
 
           <SVGCartoon className={styles['cartoon']} leftOffset={0.50} topOffset={0.1} />
-          <SVGSplat className={styles['splat1']} leftOffset={0.45} topOffset={0.05} />
-          <SVGSplat className={styles['splat2']} leftOffset={0.60} topOffset={0.2} />
+          <SVGSplat className={styles['splat1']} leftOffset={0.45} topOffset={0.05} transitionDelay={0.5} />
+          <SVGSplat className={styles['splat2']} leftOffset={0.60} topOffset={0.2} transitionDelay={1} />
+          <SVGSplat className={styles['splat3']} leftOffset={0.45} topOffset={0.33} transitionDelay={0} />
 
         </div>
 
