4df261de1483cedd7254cc25595e332175431e46
rookiemonkey <kevinroirigorbasina@pm.me>
2022-06-27 18:51:09 +0800

    :heavy_plus_sign: add a way to toggle scroll for other pages but not for home page

---
+++ src/helpers/setStyleVariable.js
@@ -0,0 +1,5 @@
+const setStyleVariable = (key, value) => {
+  document.documentElement.style.setProperty(key, value)
+}
+
+export default setStyleVariable;
--- src/hocs/Contain.js
+++ src/hocs/Contain.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useRef, useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 import { CenterScreenContext } from "../context/CenterScreen";
 import normalizeHeight from '../helpers/normalizeHeight';
+import setStyleVariable from '../helpers/setStyleVariable';
 
 const Contain = props => {
   const { setCenterScreen } = useContext(CenterScreenContext)
+  const { pathname } = useLocation()
   const _this = useRef()
 
   useEffect(() =>  {
     setCenterScreen({ x: _this.current.offsetWidth / 2, y: _this.current.offsetHeight / 2 })
     normalizeHeight(_this)
   }, [setCenterScreen])
+
+  useEffect(() => pathname === '/' ? setStyleVariable('--screen-scroll', 'hidden') : setStyleVariable('--screen-scroll', 'scroll'), [pathname])
 
   return (
     <div ref={_this} className='contain-screen'>{ props.children }</div>
