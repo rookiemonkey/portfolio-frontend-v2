d1a51d407ebf1818c1b4850e67f179f2874ac415
rookiemonkey <kevinroirigorbasina@pm.me>
2022-07-08 19:22:05 +0800

    :wrench: change skills presentation

--- src/components/Skills/SkillList.js
+++ src/components/Skills/SkillList.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import { motion } from "framer-motion";
 import SkillListItem from "~/components/Skills/SkillListItem";
 import animationVariants from "~/components/Skills/variants/skillList";
 import styles from '~/styles/Skills.module.css';
 
 const SkillList = ({ title, skills }) => {
+  const _this = useRef();
+
+  const handleHoverStart = () => _this.current.classList.add(styles['skilllist-container-hovered'])
+  const handleHoverEnd = () => _this.current.classList.remove(styles['skilllist-container-hovered'])
+
   return (
     <motion.article 
+      ref={_this}
       variants={animationVariants.listContainer}
+      onHoverStart={handleHoverStart}
+      onHoverEnd={handleHoverEnd}
       className={styles['skilllist-container']}
     >
       <h1>{ title }</h1>
