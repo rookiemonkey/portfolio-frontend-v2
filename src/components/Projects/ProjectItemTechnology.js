58427fcf0f5f0354e27f68652c767cbecce679b5
rookiemonkey <kevinroirigorbasina@pm.me>
2022-07-10 09:46:42 +0800

    :wrench: fix animation with projectitem

---
+++ src/components/Projects/ProjectItemTechnology.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { motion } from "framer-motion";
+import animationVariants from "~/components/Projects/variants/projectItem";
+import styles from '~/styles/Projects.module.css';
+
+const ProjectItemTechnology = ({ tech }) => {
+
+  return (
+    <motion.li
+      variants={animationVariants.scaleIn}
+      className={styles['project-list-item-technologies-item']}
+    >
+      <img
+        src={require(`~/assets/images/${tech.toLowerCase()}.png`)}
+        alt={tech}
+      />
+    </motion.li>
+  )
+}
+
+export default ProjectItemTechnology;
--- src/components/Projects/variants/projectItem.js
+++ src/components/Projects/variants/projectItem.js
@@ -1,6 +1,26 @@
 import variables from '~/helpers/animations';
 
+const technologies = {
+  'initial': {},
+  'animate': {
+    transition: {
+      when: "beforeChildren",
+      delayChildren: variables.values.delay_for_children,
+      staggerChildren: variables.values.delay_for_staggered_children
+    }
+  },
+  'exit': {
+    transition: {
+      when: "afterChildren",
+      staggerChildren: variables.values.delay_for_staggered_children,
+      staggerDirection: -1
+    }
+  }
+}
+
 const variants = {
+  technologies,
+  fadeInFromBottom: variables.variants.fadeInFromBottom,
   scaleIn: variables.variants.scaleIn
 }
 
