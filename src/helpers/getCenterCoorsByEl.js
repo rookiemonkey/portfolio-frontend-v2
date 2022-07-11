4bce3810a3d7fa0263c07c4ccbb81932bc240661
rookiemonkey <kevinroirigorbasina@pm.me>
2022-07-11 18:54:06 +0800

    :wrench: change some layouts

--- src/helpers/getCenterCoorsByEl.js
+++ src/helpers/getCenterCoorsByEl.js
@@ -1,8 +1,11 @@
 const getCenterCoorsByEl = (parentElement, elementToCenter) => {
+  
+  // subtracting 100px for the navigation bar
+
   if (parentElement) {
     return { 
       top: (parentElement.current.offsetHeight / 2) - (elementToCenter.current.clientHeight / 2), 
-      left: (parentElement.current.offsetWidth / 2) - (elementToCenter.current.clientWidth / 2)
+      left: ((parentElement.current.offsetWidth-100) / 2) - (elementToCenter.current.clientWidth / 2)
     }
   }
 
