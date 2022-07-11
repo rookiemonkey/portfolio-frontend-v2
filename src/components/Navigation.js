fccdff15a0ec5f6014c24f1c88a40d3f124d7eec
rookiemonkey <kevinroirigorbasina@pm.me>
2022-07-11 11:07:25 +0800

    :wrench: fix transition for nav links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,25 @@
 import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import normalizeHeight from "~/helpers/normalizeHeight";
 import styles from '~/styles/Components.module.css';
 import cartoon from '~/assets/cartoon.png';
 
 const Navigation = () => {
   const _this = useRef()
+  const navigationLinks = useRef()
 
   useEffect(() => normalizeHeight(_this), [])
+
+  const onHoverStart = ({ target }) => {
+    target.querySelector('a').classList.add('hovered')
+    navigationLinks.current.classList.add('navigation-links-hovered')
+  }
+
+  const onHoverEnd = ({ target }) => {
+    target.querySelector('a').classList.remove('hovered')
+    navigationLinks.current.classList.remove('navigation-links-hovered')
+  }
 
   return (
     <nav ref={_this} className={styles['navigation']}>
@@ -18,16 +30,24 @@ const Navigation = () => {
         </Link>
       </div>
 
-      <div className={styles['navigation-links']}>
-        <Link to='/'>
-          Home
-        </Link>
-        <Link to='/skills'>
-          Skills
-        </Link>
-        <Link to='/projects'>
-          Projects
-        </Link>
+      <div ref={navigationLinks} className={styles['navigation-links']}>
+        <motion.span onHoverStart={onHoverStart} onHoverEnd={onHoverEnd}>
+          <Link to='/'>
+            Home
+          </Link>
+        </motion.span>
+
+        <motion.span onHoverStart={onHoverStart} onHoverEnd={onHoverEnd}>
+          <Link to='/skills'>
+            Skills
+          </Link>
+        </motion.span>
+
+        <motion.span onHoverStart={onHoverStart} onHoverEnd={onHoverEnd}>
+          <Link to='/projects'>
+            Projects
+          </Link>
+        </motion.span>
       </div>
 
       <div className={styles['navigation-footer']}>
