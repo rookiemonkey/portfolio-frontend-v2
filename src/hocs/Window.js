2a6c0ea4f9bd945b3756a79cbb67f5ec1a1cebfa
rookiemonkey <kevinroirigorbasina@pm.me>
2022-07-12 09:03:10 +0800

    :wrench: remove unused vars

--- src/hocs/Window.js
+++ src/hocs/Window.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useContext, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Maintenance from "~/components/Maintenance";
 import normalizeWidth from '~/helpers/normalizeWidth';
 
